45cad40a8091e29c262e12226f5857552f8fdf65
juan arnedo <juan.arnedo@zalvadora.com>
2020-03-07 21:56:30 -0500

    use Hooks and create a custom Hook

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,36 +1,56 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from '../components/Header';
 import Search from '../components/Search';
 import Categories from '../components/Categories';
 import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CarouselItem';
 import Footer from '../components/Footer';
+import useInitialState from '../hooks/useInitialState';
 import '../assets/styles/App.scss';
 
-const App = () => (
-	<div className="App">
-		<Header />
-		<Search />
-		<Categories title="test1">
-			<Carousel>
-				<CarouselItem />
-				<CarouselItem />
-				<CarouselItem />
-			</Carousel>
-		</Categories>
-		<Categories title="test2">
-			<Carousel>
-				<CarouselItem />
-				<CarouselItem />
-			</Carousel>
-		</Categories>
-		<Categories title="test3">
-			<Carousel>
-				<CarouselItem />
-			</Carousel>
-		</Categories>
-		<Footer />
-	</div>
-);
+const API = 'http://localhost:9090/initialState';
+
+const App = () => {
+	const initialState = useInitialState(API);
+
+	return(
+		<div className="App">
+			<Header />
+			<Search />
+			{
+				initialState.mylist.length > 0 &&
+					<Categories title="Mi Lista">
+						<Carousel>
+							{
+								initialState.mylist.map( item => 
+									<CarouselItem key={ item.id } { ...item } />
+								)
+							}
+						</Carousel>
+					</Categories>
+			}
+			<Categories title="Tendencias">
+				<Carousel>
+					{
+						initialState.trends.map( item => 
+							<CarouselItem key={ item.id } { ...item } />
+						)
+					}
+				</Carousel>
+			</Categories>
+
+			<Categories title="Originales">
+				<Carousel>
+					{
+						initialState.originals.map( item => 
+							<CarouselItem key={ item.id } { ...item } />
+						)
+					}
+				</Carousel>
+			</Categories>
+			<Footer />
+		</div>
+	);
+}
 
 export default App
